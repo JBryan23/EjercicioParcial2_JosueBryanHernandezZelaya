0a648bf7b2e8fbeddab5bf69e7dcab747e4908c6
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-10-14 16:02:48 -0600

    Add files via upload

---
+++ PARCIAL 2 EJERCICIOS JOSUE BRYAN HERNANDEZ ZELAYA/Ejercicio1JBHZ/Ejercicio1JBHZ/Program.cs
@@ -0,0 +1,29 @@
+﻿// El reuter principal se le pide desarrollar un programa en c# donde permita ingresar la clave por defecto(AdministradorR)
+// para que pueda realizar las configuraciones de respaldo, a todo esto se pide que como requesito que el ingreso de la clave
+// del teclado sea independiemte de si esta activo las letras mayusculas o minusculas, tenga espacios blanco, tabulaciones, etc,
+// donde se debe hacer las transformaciones necesarias para que estas no afectan su comparacion
+
+public class Ejercicio1
+{
+    public static void Main()
+    {
+        string Clave_Por_Defecto = "AdministradorR"; 
+
+        Console.Write("Usuario porfavor ingrese la clave que se tiene por defecto: ");
+        string Clave_Del_Usuario = Console.ReadLine();
+        Clave_Del_Usuario = Clave_Del_Usuario.ToLower().Trim();
+        Clave_Por_Defecto = Clave_Por_Defecto.ToLower();
+        //Validar si la contraseña esta correcta o no para poder ingresar
+        if (Clave_Del_Usuario == Clave_Por_Defecto)
+        {
+            Console.WriteLine("Su clave es correcta tiene acceso para realizar las configuraciones de respaldo.");
+        }
+        else
+        {
+            Console.WriteLine(@"Su clave es incorrecta su acceso no sera posible para realizar las configuraciones de respaldo.
+(Asegurese que haya escrito bien la clave)");
+        }
+    }
+}
+
+//Hecho por Josue Bryan Hernandez Zelaya
---
+++ PARCIAL 2 EJERCICIOS JOSUE BRYAN HERNANDEZ ZELAYA/Ejercicio3JBHZ/Ejercicio3JBHZ/Program.cs
@@ -0,0 +1,26 @@
+﻿//La fecha de finalizacion de las actvidades academicas de la materia de Programacion 2, ciclo 2 2023 es el sabado 18 de noviembre de 2023
+// el parcial sera llevado a cabo a partir de las 3:00 Pm hacer un programa en C# que:
+// Muestre de manera automatica la fecha y hora actual del sistema
+// Muestre por seperado el numero de dias, que faltan para el parcial 3 
+// Muestre la fecha en formato 14/10/2023, 14 Oct 2023, 2023 oct 14 
+
+public class Ejercicio3
+{
+    public static void Main()
+    {
+        DateTime Fecha_Actual_del_sistema = DateTime.Now;//Ojo la del sistema 
+        DateTime Fecha_del_Parcial = new DateTime(2023, 11, 18, 15, 0, 0); // sabado 18 de noviembre de 2023 el parcial sera llevado a cabo a partir de las 3:00 Pm 
+        TimeSpan Dias_Para_Parcial = Fecha_del_Parcial - Fecha_Actual_del_sistema;
+        int Tiempo_Dias = Dias_Para_Parcial.Days;
+        Console.WriteLine("Fecha y hora actual del sistema: " + Fecha_Actual_del_sistema);
+        Console.WriteLine("Número de días restantes para el parcial 3: " + Tiempo_Dias + " dias");
+        Console.WriteLine();
+        //Fecha de formatos:
+        Console.WriteLine("Se presentas las fechas en formato: ");
+        Console.WriteLine(Fecha_Actual_del_sistema.ToString("dd/MM/yyyy"));
+        Console.WriteLine(Fecha_Actual_del_sistema.ToString("dd MMM yyyy"));
+        Console.WriteLine( Fecha_Actual_del_sistema.ToString("yyyy MMM dd"));
+    }
+}
+
+//Hecho por Josue Bryan Hernandez Zelaya
